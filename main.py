3428b2954a2a1899d53b764ff7d6041a279ee620
glwhappen <1597721684@qq.com>
2023-12-10 16:45:45 +0800

    添加查询参数

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+
+# 查询参数 q 的类型为 str，默认值为 None，因此它是可选的。
+@app.get("/items/")
+async def read_items(q: str | None = None):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
 def write_notification(email: str, message=""):
     with open("log.txt", mode="w") as email_file:
         time.sleep(10)
